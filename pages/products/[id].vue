8b3ca199f6677c8d9492d8647a1af01356ee856d
Ēriks Lapiņš <lapinseriks98@gmail.com>
2023-08-05 22:35:49 +0300

    route params

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>product details for {{ id }}</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum illum accusantium officiis, nihil est cupiditate aliquam non? Obcaecati sapiente provident beatae, ut ullam sint laudantium aperiam ea perspiciatis odit officiis.</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params;
+</script>
+
+<style scoped>
+
+</style>
