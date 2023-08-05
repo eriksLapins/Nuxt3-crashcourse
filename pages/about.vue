dba44715314def9a8f885e1fd2b976710f5e2614
Ēriks Lapiņš <lapinseriks98@gmail.com>
2023-08-05 22:15:04 +0300

    pages and subpages

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dolores beatae tenetur nisi. Magnam labore beatae molestias? Numquam repellendus laboriosam aliquid, enim corporis deleniti, impedit rem consectetur error commodi nisi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dolores beatae tenetur nisi. Magnam labore beatae molestias? Numquam repellendus laboriosam aliquid, enim corporis deleniti, impedit rem consectetur error commodi nisi!</p>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p {
+        margin: 20px 0;
+    }
+</style>
