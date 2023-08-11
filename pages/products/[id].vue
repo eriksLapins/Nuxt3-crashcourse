6aaa484edf2b431eb1c4882d6db81e68d9e54f81
Ēriks Lapiņš <lapinseriks98@gmail.com>
2023-08-11 22:26:35 +0300

    error handling

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -14,6 +14,11 @@
     // fetch the data and each time we land on a new product, we will fetch again (that is why we have the key)
         // otherwise fetch will use the previous fetched data
     const { data: product } = await useFetch(uri, { key: id });
+
+    if (!product.value) {
+        // fatal: true forces the page to go into errorpage also in unhandled situations
+        throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true })
+    }
 </script>
 
 <style scoped>
